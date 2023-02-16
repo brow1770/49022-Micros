c08ab5ae13cba76b66564ff50b1f1e8b36850552
Ben Brown <Ben_Brown19@Bens-MacBook-Pro.local>
2023-02-16 18:54:38 -0500

    added server capability

--- main/whacker.c
+++ main/whacker.c
@@ -11,20 +11,51 @@
 #include "nimble/nimble_port_freertos.h"
 #include "host/ble_hs.h"
 #include "services/gap/ble_svc_gap.h"
+#include "services/gatt/ble_svc_gatt.h"
+#include "sdkconfig.h"
 
 
-char *TAG = "BLE Client Scan";
+char *CLIENT_TAG = "BLE Client Scan";
+char *SERVER_TAG= "BLE Server Advertise";
 uint8_t ble_addr_type;
 void ble_app_scan(void);
+void ble_app_advertise(void);
+
+//write data
+static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    printf("Data from the client: %.*s\n", ctxt->om->om_len, ctxt->om->om_data);
+    return 0;
+}
+
+//read data
+static int device_read(uint16_t con_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    os_mbuf_append(ctxt->om, "Data from the server", strlen("Data from the server"));
+    return 0;
+}
+
+static const struct ble_gatt_svc_def gatt_svcs[] = {
+    {.type = BLE_GATT_SVC_TYPE_PRIMARY,
+     .uuid = BLE_UUID16_DECLARE(0x180),                 //Define UUID for device type
+     .characteristics = (struct ble_gatt_chr_def[]){
+         {.uuid = BLE_UUID16_DECLARE(0xFEF4),           //Define UUID for reading
+          .flags = BLE_GATT_CHR_F_READ,
+          .access_cb = device_read},
+         {.uuid = BLE_UUID16_DECLARE(0xDEAD),           //Define UUID for writing
+          .flags = BLE_GATT_CHR_F_WRITE,
+          .access_cb = device_write},
+         {0}}},
+    {0}
+};
 
 // BLE event handling
-static int ble_gap_event(struct ble_gap_event *event, void *arg)
+static int ble_gap_event_client(struct ble_gap_event *event, void *arg)
 {
     struct ble_hs_adv_fields fields;
 
     switch (event->type)
     {
-    // NimBLE event discovery
     case BLE_GAP_EVENT_DISC:
         ESP_LOGI("GAP", "GAP EVENT DISCOVERY");
         ble_hs_adv_parse_fields(&fields, event->disc.data, event->disc.length_data);
@@ -39,6 +70,49 @@ static int ble_gap_event(struct ble_gap_event *event, void *arg)
     return 0;
 }
 
+static int ble_gap_event_server(struct ble_gap_event *event, void *arg)
+{
+    switch (event->type)
+    {
+    // Advertise if connected
+    case BLE_GAP_EVENT_CONNECT:
+        ESP_LOGI("GAP", "BLE GAP EVENT CONNECT %s", event->connect.status == 0 ? "OK!" : "FAILED!");
+        if (event->connect.status != 0)
+        {
+            ble_app_advertise();
+        }
+        break;
+    // Advertise again after completion of the event
+    case BLE_GAP_EVENT_ADV_COMPLETE:
+        ESP_LOGI("GAP", "BLE GAP EVENT");
+        ble_app_advertise();
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+void ble_app_advertise(void)
+{
+    // GAP - device name definition
+    struct ble_hs_adv_fields fields;
+    const char *device_name;
+    memset(&fields, 0, sizeof(fields));
+    device_name = ble_svc_gap_device_name(); // Read the BLE device name
+    fields.name = (uint8_t *)device_name;
+    fields.name_len = strlen(device_name);
+    fields.name_is_complete = 1;
+    ble_gap_adv_set_fields(&fields);
+
+    // GAP - device connectivity definition
+    struct ble_gap_adv_params adv_params;
+    memset(&adv_params, 0, sizeof(adv_params));
+    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND; // connectable or non-connectable
+    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN; // discoverable or non-discoverable
+    ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event_server, NULL);
+}
+
 void ble_app_scan(void)
 {
     printf("Start scanning ...\n");
@@ -51,11 +125,17 @@ void ble_app_scan(void)
     disc_params.filter_policy = 0;
     disc_params.limited = 0;
 
-    ble_gap_disc(ble_addr_type, BLE_HS_FOREVER, &disc_params, ble_gap_event, NULL);
+    ble_gap_disc(ble_addr_type, BLE_HS_FOREVER, &disc_params, ble_gap_event_client, NULL);
+}
+
+void ble_app_on_sync_server(void)
+{
+    ble_hs_id_infer_auto(0, &ble_addr_type); // Determines the best address type automatically
+    ble_app_advertise();                     // Define the BLE connection
 }
 
 // The application
-void ble_app_on_sync(void)
+void ble_app_on_sync_client(void)
 {
     ble_hs_id_infer_auto(0, &ble_addr_type); // Determines the best address type automatically
     ble_app_scan();                          
@@ -67,30 +147,35 @@ void host_task(void *param)
     nimble_port_run(); // This function will return only when nimble_port_stop() is executed
 }
 
+void run_client(void){
+    ble_svc_gap_device_name_set("ESP-32-BLE"); 
+    ble_svc_gap_init();                       
+    ble_hs_cfg.sync_cb = ble_app_on_sync_client;    
+    nimble_port_freertos_init(host_task);   //run thread
+}
+
+void run_server(void){
+    ble_svc_gap_device_name_set("BLE-Server"); 
+    ble_svc_gap_init();                       
+    ble_svc_gatt_init();                    
+    ble_gatts_count_cfg(gatt_svcs);        
+    ble_gatts_add_svcs(gatt_svcs);        
+    ble_hs_cfg.sync_cb = ble_app_on_sync_server; 
+    nimble_port_freertos_init(host_task); 
+}
+
 void app_main()
 {
     nvs_flash_init();                               // 1 - Initialize NVS flash using
     nimble_port_init();                             // 3 - Initialize the controller stack
-    ble_svc_gap_device_name_set("BLE-Scan-Client"); // 4 - Set device name characteristic
+   /* ble_svc_gap_device_name_set("ESP-32-BLE"); // 4 - Set device name characteristic
     ble_svc_gap_init();                             // 4 - Initialize GAP service
-    ble_hs_cfg.sync_cb = ble_app_on_sync;           // 5 - Set application
-    nimble_port_freertos_init(host_task);           // 6 - Set infinite task
-}
-
-/*#define BLINK_LED 13
-
-void app_main(void)
-{
-    char *taskName = pcTaskGetName(NULL);
-    ESP_LOGI(taskName, "Hello World");
-
-    gpio_reset_pin(BLINK_LED);
-    gpio_set_direction(BLINK_LED, GPIO_MODE_OUTPUT);
     
-    while(1){
-        gpio_set_level(BLINK_LED, 1);
-        vTaskDelay(1000/ portTick_PERIOD_MS);
-        gpio_set_level(BLINK_LED, 0);
-        vTaskDelay(1000/ portTick_PERIOD_MS);
-    }
-}*/
+    //Client
+    ble_hs_cfg.sync_cb = ble_app_on_sync;           // 5 - Set application
+    nimble_port_freertos_init(host_task);           // 6 - Set infinite task*/
+    //run_client();
+    
+    //BLE Server
+    run_server();
+}
